18eabf6ed7b577efc4a9c897c7603f314c92f112
sjvenkatesh14-cloud <114215598+JaiVenkatesh14@users.noreply.github.com>
2026-08-11 10:16:33 +0530

    removed few unwanted texts buttons

--- Kptcl_Excel_Load/obj/Debug/net10.0/Kptcl_Excel_Load.AssemblyInfo.cs
+++ Kptcl_Excel_Load/obj/Debug/net10.0/Kptcl_Excel_Load.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kptcl_Excel_Load")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f68695950652d70f03e7de07b64101ae5ea561c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b8cb7e1ac46fb35c59f5a822b38d8f71573597")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kptcl_Excel_Load")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kptcl_Excel_Load")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
